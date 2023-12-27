596587c9862a0ed4702c23dde70086679d2468e2
MeetSondagar <meetsondagar030905@gmail.com>
2023-12-27 18:59:21 +0530

    About page Done✅

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const menuBtn = document.querySelector(".menu-btn");
 const hamBurger = document.querySelector(".menu-btn_burger");
+const nav = document.querySelector(".nav");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".menu-nav_item");
 
 // let showMenu = false;
 
@@ -7,9 +10,11 @@ menuBtn.addEventListener("click", ToggleMenu);
 
 function ToggleMenu() {
   hamBurger.classList.toggle("open");
+  menuNav.classList.toggle("open");
+  nav.classList.toggle("open");
+  navItems.forEach((item) => item.classList.toggle("open"));
   // if (!showMenu) {
-  // // } else {
-  // //   hamBurger.classList.remove("open");
+  // } else {
   //   showMenu = false;
   // }
 }
